bc510ec043a914c4a3fa334afeea36b916bf66ba
Isac Vieira <isacvieira2009@hotmail.com>
2023-08-02 16:54:26 -0300

    apply changes

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const card = document.querySelector(".card__escondido");
-const botao = document.querySelector(".botao");
+const botao = document.querySelector(".botao__abrir");
+const botaoFechar = document.querySelector(".botao__fechar");
+const ajusteDisplay = document.querySelector("body");
 
 botao.addEventListener("click", () => {
   card.style.display = "block";
+  botao.style.display = "none";
+});
+
+botaoFechar.addEventListener("click", () => {
+  card.style.display = "none";
+  botao.style.display = "block";
 });
