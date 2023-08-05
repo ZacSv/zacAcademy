3b82ceaa511dc53ff345b33b6493813cc0361738
Isac Vieira <isacvieira2009@hotmail.com>
2023-08-05 16:01:37 -0300

    create a button of close cards

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function abrirFecharElemento(targetId) {
   const elemento = document.getElementById(targetId);
-  elemento.style.display = elemento.style.display === "none" ? "flex" : "none";
+  elemento.style.display = "flex";
+  // elemento.style.display = elemento.style.display === "none" ? "flex" : "none";
 }
 
 function fecharModal(targetId) {
@@ -10,6 +11,14 @@ function fecharModal(targetId) {
 
 document.addEventListener("click", (event) => {
   const botao = event.target;
+  const botaoFechar = event.target;
+  console.log(botao);
+  console.log(botaoFechar);
+
+  if (botaoFechar.classList.contains("icone-fechar")) {
+    const targetFechamento = botao.getAttribute("data-target");
+    fecharModal(`modal-${targetFechamento}`);
+  }
   if (botao.classList.contains("botao-abrir")) {
     const targetId = botao.getAttribute("data-target");
     abrirFecharElemento(`modal-${targetId}`);
