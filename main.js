fadbe347bd0ee4313543f38ee21f3a6a9c45bbc6
Isac Vieira <isacvieira2009@hotmail.com>
2023-08-05 01:22:10 -0300

    finalyzing create a cards

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
-const card = document.querySelectorAll(".card__escondido");
-const botao = document.querySelectorAll(".botao__abrir");
-const botaoFechar = document.querySelector(".botao__fechar");
-
-function mudaDisplay(elemento) {
-  document.querySelector(elemento).style.display = "inline-block";
+function abrirFecharElemento(targetId) {
+  const elemento = document.getElementById(targetId);
+  elemento.style.display = elemento.style.display === "none" ? "flex" : "none";
 }
 
-function fechaJanela(elemento) {
-  document.querySelector(elemento).style.display = "none";
+function fecharModal(targetId) {
+  const modal = document.getElementById(targetId);
+  modal.style.display = "none";
 }
 
-for (var index = 0; index < botao.length; index++) {
-  const exercicio = botao[index].classList[1];
-  const itemPositionBotao = botao[index];
-
-  for (var i = 0; i < card.length; i++) {
-    const cardIterado = card[i].classList[0];
+document.addEventListener("click", (event) => {
+  const botao = event.target;
+  if (botao.classList.contains("botao-abrir")) {
+    const targetId = botao.getAttribute("data-target");
+    abrirFecharElemento(`modal-${targetId}`);
   }
-  itemPositionBotao.onclick = function () {
-    mudaDisplay("#card__escondido__" + exercicio);
-  };
-}
+});
